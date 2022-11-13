401982af513832519d42d813628c90a4a90da5cf
DimonIPymba <nazvanie5486@gmail.com>
2022-11-13 09:54:24 +0200

    Добавьте файлы проекта.

---
+++ Point/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure figure = new Figure(new Point(1, 1, "A"), new Point(4, 1, "B"), new Point(4, 5, "C"));
+            Console.WriteLine(figure.PerimeterCalculator());
+        }
+    }
+}
